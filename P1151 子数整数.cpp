846dd802a5043d335522b61d93ace1d6f4f25c1a
zot_furry <a2416333938@outlook.com>
2026-08-15 11:47:22 +0800

    添加

--- P1151 子数整数.cpp
+++ P1151 子数整数.cpp
@@ -1,5 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-
-
+int k;
+bool jianc(int k,string u){
+	int a,b,c;
+	a=stoi(u.substr(0, 3));
+	b=stoi(u.substr(1, 3));
+	c=stoi(u.substr(2, 3));
+	return a%k+b%k+c%k==0;
+	
+}
+int main(){
+	cin>>k;
+	for(int i=10000;i<=30000;i++){
+		if(jianc(k,to_string(i))){
+			cout<<i;
+		}
+	}
+}
